a71e35cbb11f47bee171cd7c58a90c03db47c754
Harshithpatali <harshikollur302@gmail.com>
2026-07-04 20:28:39 +0530

    debug ask endpoint

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
+import traceback
 
-app = FastAPI(
-    title="TCS Financial Intelligence Agent"
-)
+app = FastAPI(title="TCS Financial Intelligence Agent")
 
 @app.get("/")
 def root():
@@ -10,11 +9,20 @@ def root():
 
 @app.get("/ask")
 def ask(question: str):
+    try:
+        print("Importing rag_pipeline")
 
-    print("Importing rag_pipeline")
+        from backend.rag.rag_pipeline import answer_question
 
-    from backend.rag.rag_pipeline import answer_question
+        print("rag_pipeline imported")
 
-    print("rag_pipeline imported")
+        return answer_question(question)
 
-    return answer_question(question)
+    except Exception as e:
+        print("ERROR IN /ask")
+        print(str(e))
+        traceback.print_exc()
+
+        return {
+            "error": str(e)
+        }
