d214df732bcc0c6e5086b2c931814801794c94fe
Harshithpatali <harshikollur302@gmail.com>
2026-07-04 17:38:01 +0530

    test render startup

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,23 +1,15 @@
 from fastapi import FastAPI
-#from backend.rag.rag_pipeline import answer_question
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"status": "ok"}
-
+from backend.rag.rag_pipeline import answer_question
 
 app = FastAPI(
     title="TCS Financial Intelligence Agent"
 )
 
-#@app.get("/ask")
-#def ask(question: str):
+@app.get("/ask")
+def ask(question: str):
 
-    #result = answer_question(
-        #question
-    #)
+    result = answer_question(
+        question
+    )
 
-    #return result
+    return result
