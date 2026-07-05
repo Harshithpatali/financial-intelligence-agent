45a22b5ba06e6b9359553a27e9d046350b459167
Harshithpatali <harshikollur302@gmail.com>
2026-07-05 11:34:42 +0530

    Pin httpx 0.27.2

--- backend/rag/llm.py
+++ backend/rag/llm.py
@@ -1,41 +1,33 @@
-import os
-from groq import Groq
-
 print("llm.py imported")
 
+from groq import Groq
+import os
+
+print("Creating Groq client")
+
 client = Groq(
-    api_key=os.getenv(
-        "GROQ_API_KEY"
-    )
+    api_key=os.getenv("GROQ_API_KEY")
 )
+
+print("Groq client created")
 
 
 def generate_answer(prompt):
 
-    print(
-        "Sending prompt to Groq..."
+    print("Sending prompt to Groq")
+
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        temperature=0.1,
+        max_tokens=1200
     )
 
-    response = (
-        client.chat.completions.create(
-            model="llama-3.3-70b-versatile",
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
-            temperature=0.1,
-            max_tokens=1200
-        )
-    )
+    print("Groq response received")
 
-    print(
-        "Groq response received"
-    )
-
-    return (
-        response
-        .choices[0]
-        .message.content
-    )
+    return response.choices[0].message.content
